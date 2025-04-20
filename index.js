1f84b3fddee562ebbc52a31578f33aeb1472b699
tsifrin <tsifrin@gmail.com>
2025-04-20 14:21:45 -0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-black via-gray-900 to-black">
-      <h1 className="text-white text-7xl font-bold">twe.lv</h1>
-    </div>
-  );
-}
